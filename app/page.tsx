ca14124caaff3bb07547b70c78a162c7e7f87667
CodeArcade <admin@codearcade.com>
2023-08-20 19:11:07 -0400

    Final

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,27 @@
-export default function HomePage() {
-  return <div>Home</div>;
+import Post from "@/components/Post";
+import prisma from "@/lib/prisma";
+
+// export const dynamic = "force-dynamic"
+
+export default async function HomePage() {
+  const posts = await prisma.post.findMany({
+    where: {
+      published: true,
+    },
+    orderBy: {
+      updatedAt: "desc",
+    },
+    include: {
+      author: true,
+    },
+  });
+
+  return (
+    <main>
+      <h1>Latest Posts</h1>
+      {posts.map((post) => (
+        <Post key={post.id} post={post} />
+      ))}
+    </main>
+  );
 }
